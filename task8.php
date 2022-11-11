589b0159c6f4a87ccd80adbd26689ba438bbf246
Bibit Kukuh Wicaksono <bibitkukuh00@gmail.com>
2022-11-11 17:21:02 +0700

    latihan function php

---
+++ task8.php
@@ -0,0 +1,10 @@
+<?php
+function perkalian($angka1, $angka2)
+{
+    echo ($angka1*$angka2)."<br/>"; 
+    echo ($angka1*$angka2)."<br/>"; 
+}
+$hasil = perkalian(20,3);
+echo "Perkalian 20 x 3 adalah $hasil";
+echo "<br>";
+echo "Perkalian 9 x 2 adalah" . perkalian (9, 2);
